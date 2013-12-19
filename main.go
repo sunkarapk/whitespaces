c846a7e3e77ff96d812594dc91b6d482994eefe3
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2013-12-20 03:43:35 +0530

    Finished main program

--- main.go
+++ main.go
@@ -3,32 +3,37 @@ package main
 import (
 	"fmt"
 	"github.com/jessevdk/go-flags"
+	"io/ioutil"
 	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
 )
 
 const (
 	Version = "0.1.0"
 )
 
+type Options struct {
+	Version bool `short:"v" long:"version" description:"Show version information"`
+	Check   bool `short:"c" long:"check" description:"Check instead of overwriting"`
+	Force   bool `short:"f" long:"force" description:"Force whitespace checking all files"`
+}
+
 func main() {
 
 	// Options for flags package
-	var opts struct {
-		Version bool `short:"v" long:"version" description:"Show version information"`
-	}
+	var opts Options
 
 	// Build the parser
 	parser := flags.NewParser(&opts, flags.Default)
 
 	// Set usage string
-	parser.Usage = "[options] [FILES]"
+	parser.Usage = "[options] FILES"
 
 	// Parse the arguments
 	args, err := parser.Parse()
-
-	if err != nil {
-		os.Exit(0)
-	}
+	handle(err)
 
 	// Print version and exit
 	if opts.Version {
@@ -39,6 +44,58 @@ func main() {
 	// If no argument is given
 	if len(args) == 0 {
 		parser.WriteHelp(os.Stdout)
+		os.Exit(0)
+	}
+
+	whitespace := regexp.MustCompile("\\s+$")
+
+	for _, v := range args {
+
+		name, err := filepath.Abs(v)
+		handle(err)
+
+		info, err := os.Stat(name)
+		handle(err)
+
+		if info.IsDir() {
+			fmt.Println(name + " seems to be a directory. Skipping...")
+		} else {
+			data, err := ioutil.ReadFile(name)
+			handle(err)
+
+			lines := strings.Split(string(data), "\n")
+
+			for i, line := range lines {
+				if opts.Check {
+					if whitespace.FindStringIndex(line) != nil {
+						check(name, opts)
+						break
+					}
+				} else {
+					lines[i] = whitespace.ReplaceAllString(line, "")
+				}
+			}
+
+			if !opts.Check {
+				data := []byte(strings.Join(lines, "\n"))
+
+				ioutil.WriteFile(name, data, info.Mode())
+			}
+		}
+	}
+}
+
+func check(name string, opts Options) {
+	fmt.Println("Found whitespace in " + name)
+
+	if !opts.Force {
+		os.Exit(0)
+	}
+}
+
+func handle(err error) {
+	if err != nil {
+		fmt.Println(err)
 		os.Exit(0)
 	}
 }
